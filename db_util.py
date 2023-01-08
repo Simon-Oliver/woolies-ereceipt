29c2a3de6475c71f00856cf33c132ce82a700c72
Simon-Oliver <stauffer.simon@outlook.com>
2023-01-09 10:57:37 +1100

    Refactored the get_data function. Seperated the code into a get_branch_data, get_list_of_line_items and added a function to proberly parse the date into iso format

--- db_util.py
+++ db_util.py
@@ -1,5 +1,6 @@
 import sqlite3
 import json
+from datetime import datetime
 
 conn = sqlite3.connect("data/receipt_data.db")
 c = conn.cursor()
@@ -55,14 +56,64 @@ def get_receipts_from_response(response):
     return items
 
 
-def get_data(receipt_id, items):
-    total = 0
-    # store_no,title,content,division
-    store_no = ''
-    title = ''
-    content = ''
-    division = ''
+def get_branch_data(item):
+    keys = ['storeNo', 'title', 'content', 'division']
+    return list(item[key] for key in keys)
 
+
+def get_list_of_line_items(receipt_id, item):
+    skip_to = 0
+    receipt_items = []
+    raw_data = json.dumps(item)
+    for i, e in enumerate(item['items']):
+        item_qty = 0
+        item_unit = ''
+        if i < skip_to:
+            continue
+        if e['amount'] == '':
+            skip_to = i + 2
+            try:
+                next_item = item['items'][i + 1]
+            except IndexError as index_error:
+                print(i, item['items'])
+                print(e)
+                if 'PRICE REDUCED' in e['description']:
+                    break
+                else:
+                    print(index_error)
+            if 'PRICE REDUCED' in e['description']:
+                skip_to = i + 1
+                continue
+
+            if 'Qty' in next_item['description']:
+                item_qty = float(next_item['description'].split(" ")[1])
+            elif 'NET @' in next_item['description']:
+                item_qty = float(next_item['description'].split(" ")[0])
+                item_unit = next_item['description'].split(" ")[1]
+
+            item_name = e['description']
+            item_total = float(next_item['amount'])
+
+            receipt_items.append((receipt_id, item_name, item_qty, item_unit, item_total, raw_data))
+
+        elif 'PRICE REDUCED' in e['description']:
+            skip_to = i + 1
+            continue
+        else:
+            item_name = e['description']
+            item_total = float(e['amount'])
+            item_qty = 1
+            item_unit = ''
+
+            receipt_items.append((receipt_id, item_name, item_qty, item_unit, item_total, raw_data))
+
+    return receipt_items
+
+def get_iso_date_from_string(date_str:str):
+    d = datetime.strptime(date_str, '%d/%m/%Y')
+    return d.strftime("%Y-%m-%d")
+
+def get_data(receipt_id, items):
     # receipt_id,receipt_date,receipt_total,store_no,raw_data
     receipt_date = ''
     receipt_total = ''
@@ -72,66 +123,22 @@ def get_data(receipt_id, items):
     receipt_items = []
 
     try:
+        receipt_url = items['data']['receiptDetails']['download']['url']
         for item in items['data']['receiptDetails']['details']:
-            # print(json.dumps(item, indent=4))
             if item['__typename'] == 'ReceiptDetailsHeader':
-                store_no = item['storeNo']
-                title = item['title']
-                content = item['content']
-                division = item['division']
+                branch = get_branch_data(item)
+                store_no = branch[0]
             elif item['__typename'] == 'ReceiptDetailsTotal':
                 receipt_total = float(item['total'][1:])
             elif item['__typename'] == 'ReceiptDetailsFooter':
-                receipt_date = item['transactionDetails'][-10:]
+                receipt_date = get_iso_date_from_string(item['transactionDetails'][-10:])
             elif item['__typename'] == 'ReceiptDetailsItems':
-                skip_to = 0
-                for i, e in enumerate(item['items']):
-                    item_qty = 0
-                    item_unit = ''
-                    if i < skip_to:
-                        continue
-                    if e['amount'] == '':
-                        skip_to = i + 2
-                        try:
-                            next_item = item['items'][i + 1]
-                        except IndexError as e:
-                            if 'PRICE REDUCED' in e['description'] or next_item['description']:
-                                break
-                            else:
-                                print(e)
-                        if 'PRICE REDUCED' in e['description']:
-                            skip_to = i + 1
-                            continue
-
-                        if 'Qty' in next_item['description']:
-                            item_qty = float(next_item['description'].split(" ")[1])
-                        elif 'NET @' in next_item['description']:
-                            item_qty = float(next_item['description'].split(" ")[0])
-                            item_unit = next_item['description'].split(" ")[1]
-
-                        item_name = e['description']
-                        item_total = float(next_item['amount'])
-
-                        receipt_items.append((receipt_id, item_name, item_qty, item_unit, item_total, raw_data))
-                        total += float(next_item['amount'])
-
-                    elif 'PRICE REDUCED' in e['description']:
-                        skip_to = i + 1
-                        continue
-                    else:
-                        item_name = e['description']
-                        item_total = float(e['amount'])
-                        item_qty = 1
-                        item_unit = ''
-
-                        receipt_items.append((receipt_id, item_name, item_qty, item_unit, item_total, raw_data))
-                        total += float(e['amount'])
+                receipt_items = get_list_of_line_items(receipt_id, item)
 
     except KeyError as e:
         with open('data/error.txt', 'a+') as error_file:
             error_file.writelines(f'{receipt_id} - Key error at {e}\n')
 
-    branch = (store_no, title, content, division)
     receipt_info = (receipt_id, receipt_url, receipt_date, receipt_total, store_no, raw_data)
 
     return [branch, receipt_info, receipt_items]
